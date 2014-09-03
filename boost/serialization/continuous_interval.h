fa0289028ec1372792ef8eec594b0ed3890eed3c
Mitja Kleider <mitja.kleider@kip.uni-heidelberg.de>
2014-09-03 16:50:31 +0200

    add serialization for boost::icl::continuous_interval

---
+++ boost/serialization/continuous_interval.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <boost/serialization/split_free.hpp>
+#include <boost/icl/continuous_interval.hpp>
+#include <boost/serialization/utility.hpp>
+
+namespace boost {
+namespace serialization {
+
+    template <typename Archive, typename V>
+        void save(Archive& ar, boost::icl::continuous_interval<V> const& di, unsigned) {
+            auto bb = di.bounds().bits();
+            V l  = di.lower();
+            V u  = di.upper();
+
+            ar << BOOST_SERIALIZATION_NVP(bb)
+			   << BOOST_SERIALIZATION_NVP(l)
+			   << BOOST_SERIALIZATION_NVP(u);
+        }
+
+    template <typename Archive, typename V>
+        void load(Archive& ar, boost::icl::continuous_interval<V>& di, unsigned) {
+            auto bb = di.bounds().bits();
+            V    l, u;
+
+            ar >> BOOST_SERIALIZATION_NVP(bb)
+			   >> BOOST_SERIALIZATION_NVP(l)
+			   >> BOOST_SERIALIZATION_NVP(u);
+
+			di = boost::icl::continuous_interval<V>(l, u, boost::icl::interval_bounds(bb));
+        }
+
+    template <typename Archive, typename V>
+        void serialize(Archive& ar, boost::icl::continuous_interval<V>& di, unsigned v) {
+            split_free(ar, di, v);
+        }
+
+} // serialization
+} //boost
