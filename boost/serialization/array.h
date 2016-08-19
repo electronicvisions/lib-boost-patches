da2d3f458c419f229759d7a17c2303be25f9e289
Bernhard Vogginger <Bernhard.Vogginger@tu-dresden.de>
2016-08-19 11:06:14 +0200

    Restrict std::array serialization patch to boost versions < 1.56
    Since 1.56 boost natively supports std::array serialization:
    https://github.com/boostorg/serialization/blob/boost-1.56.0/include/boost/serialization/array.hpp#L145
    
    Change-Id: I317c5bae918b3638782c22a36c31996817181fde

--- boost/serialization/array.h
+++ boost/serialization/array.h
@@ -1,13 +1,15 @@
 #pragma once
 
 #include <boost/serialization/array.hpp>
+#include <boost/version.hpp>
 
 namespace std {
 template<typename _Tp, std::size_t _Nm>
 struct array;
 }
 
-// serialization helper for std::array
+#if BOOST_VERSION < 105600
+// serialization helper for std::array for boost version < 1.56
 namespace boost {
 namespace serialization {
 
@@ -19,3 +21,4 @@ void serialize(Archiver & ar, std::array<T, Size> & s, unsigned int const versio
 
 } // namespace serialization
 } // namespace boost
+#endif
