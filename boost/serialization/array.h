3646c749edbb792d6273d7eafc9d643e1b33000b
Eric Müller <mueller@kip.uni-heidelberg.de>
2017-11-13 16:06:22 +0100

    Fix build on emscripten
    Change-Id: If1cf21d7f518180b8d77567a0aee4849b55385e1

--- boost/serialization/array.h
+++ boost/serialization/array.h
@@ -3,10 +3,14 @@
 #include <boost/serialization/array.hpp>
 #include <boost/version.hpp>
 
+#ifdef PYPLUSPLUS
 namespace std {
 template<typename _Tp, std::size_t _Nm>
 struct array;
 }
+#else
+#include <array>
+#endif
 
 #if BOOST_VERSION < 105600
 // serialization helper for std::array for boost version < 1.56
